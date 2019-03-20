eb34f266f9fdc8bc603bf2cec47dc1a4c7a846a4
marianomastronardi <40776619+marianomastronardi@users.noreply.github.com>
2019-03-19 22:15:06 -0300

    Add files via upload

---
+++ LaboratorioII/Ejercicio02/ClassEjercicio02.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    class ClassEjercicio02
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 02";
+            //string formatodecimal = "{0,10:#,###.00}";
+            int iInput = 0;
+            bool repeat = false;
+
+            while(!repeat)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                repeat = int.TryParse(Console.ReadLine(), out iInput);
+
+                if (iInput <= 0)
+                {
+                    Console.WriteLine("ERROR Reingrese el numero!");
+                    Console.ReadKey();
+                    repeat = false;
+                }
+                else
+                {
+                    Console.WriteLine("Su numero ingresado al cuadrado es: " + Math.Pow((double)iInput, 2));
+                    Console.WriteLine("Su numero ingresado al cuadrado es: " + Math.Pow((double)iInput, 3));
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
