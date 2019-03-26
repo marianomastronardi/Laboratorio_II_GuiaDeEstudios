27f798ab3b31b1e781f3a4d7d5c2eaeb25f9587c
marianomastronardi <40776619+marianomastronardi@users.noreply.github.com>
2019-03-26 13:14:02 -0300

    Add files via upload

---
+++ GuiaDeEjercicios/Ejercicio06/ClassEjercicio06.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio06
+{
+    class ClassEjercicio06
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
